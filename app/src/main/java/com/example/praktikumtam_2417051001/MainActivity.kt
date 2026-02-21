b59161135b01caf7e64f50b32d24e5207d9ec55e
chayyarra <chayya.igda.897@gmail.com>
2026-02-21 12:41:54 +0700

    Initial commit: setup modul 2

---
+++ app/src/main/java/com/example/praktikumtam_2417051001/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.praktikumtam_2417051001
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.praktikumtam_2417051001.ui.theme.PraktikumTAM_2417051001Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PraktikumTAM_2417051001Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Chayyarra Igda Prameswari",
+                        npm = "2417051001",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier, npm: String) {
+    Text(
+        text = "Hello, saya $name dengan NPM $npm siap belajar compose!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PraktikumTAM_2417051001Theme {
+        Greeting(name = "Chayyarra Igda Prameswari", npm = "2417051001")
+    }
+}
